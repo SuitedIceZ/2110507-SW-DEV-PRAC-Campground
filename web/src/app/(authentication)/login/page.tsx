10e9295cd028d58f6383c00ed2f0919d2b4c852b
SuitedIceZ <okshopm575@gmail.com>
2023-11-22 15:30:59 +0700

    feat: auth context (wait to fix client async)

--- web/src/app/(authentication)/login/page.tsx
+++ web/src/app/(authentication)/login/page.tsx
@@ -1,13 +1,35 @@
+"use client"
+import styles from "./page.module.css"
+import globalStyles from "../../Global.module.css"
+import { Button } from "@mui/material";
+import useLoginForm from "./useLoginForm";
+import TextField from "../../../components/TextField";
+
 export default function LoginPage() {
+  const {
+    email,
+    password,
+    handleSubmit,
+    handleEmailChange,
+    handlePasswordChange,
+  } = useLoginForm();
+
     return (
-      <main>
-        <div>
-          <p>Campground</p>
+      <div className={styles.pageContainer}>
+      <div className={styles.titleContainer}>
+          <p className={styles.header1Title}>Campground</p>
+          <p className={styles.header2Title}>Booking</p>
+      </div>
           <div>
-            <p>Login</p>
-            <form></form>
+            <p className={styles.header3Title}>Login</p>
+              <form className={styles.FormContainer} onSubmit={handleSubmit}>
+                <TextField  label="Email" className="field" autoComplete='off' value={email} onChange={handleEmailChange}/>
+                <TextField  label="Password" className="field" autoComplete='off' value={password} onChange={handlePasswordChange}/>
+                <div className="flex center">
+                  <Button type='submit' variant="outlined" color='inherit'>Login</Button>
+                </div>
+              </form>
           </div>
-        </div>
-      </main>
+      </div>
     )
   }
---
+++ web/src/contexts/authContext.tsx
@@ -0,0 +1,69 @@
+'use client'
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import {IAuthContext} from './types'
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  tel: string;
+  role: string;
+};
+
+const removeToken = (): void => {
+    localStorage.removeItem('token');   
+};
+  
+const AuthContext = createContext<IAuthContext>({} as IAuthContext)
+
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [token, setToken] = useState(localStorage.getItem('token') || "");
+
+  useEffect(() => {
+    //const token = localStorage.getItem('token');
+    setToken(localStorage.getItem('token') || "");
+
+    const validateToken = async () => {
+        console.log("validating Token")
+      try {
+        const res = await fetch(process.env['NEXT_PUBLIC_GATEWAY_URL'] + '/api/v1/auth/me', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+        })
+        const data = await res.json();
+        if(data.success) {
+            console.log("data.success data.name : ",data.data.name)
+            setName(data.data.name);
+            setRole(data.data.role);
+            setToken(data.data.token);
+        }
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+
+    if(token)
+      validateToken();
+  }, [])
+
+  const logout = () => {
+    setName("");
+    setRole("");
+    setToken("");
+    removeToken();
+  };
+
+  return (
+    <AuthContext.Provider value={{name, setName, role, setRole, token, setToken, logout}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
